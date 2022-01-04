a026ce46bfdd58ae8f89bcdc2a467c8bbfa4a849
Sumanth Keshav <Sumanth Keshav@TYSS-Sumanth>
2022-01-04 10:31:37 +0530

    First Commit

---
+++ JenkinsDemo/src/test/java/sampleTestScripts/TC_Accounts_001Test.java
@@ -0,0 +1,24 @@
+package sampleTestScripts;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC_Accounts_001Test {
+
+	@Test
+	public void createAccount() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://selenium08.blogspot.com/");
+		
+		Reporter.log("Created Account successfully");
+	}
+}
