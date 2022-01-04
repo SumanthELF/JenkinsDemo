64050a803cd64a04237cb1580bbca6bc7c5f17de
Sumanth Keshav <Sumanth Keshav@TYSS-Sumanth>
2022-01-04 12:09:34 +0530

    Added a test script TC_Loans002Test

---
+++ JenkinsDemo/src/test/java/sampleTestScripts/TC_Loans_002Test.java
@@ -0,0 +1,12 @@
+package sampleTestScripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC_Loans_002Test {
+
+	@Test
+	public void print() {
+		Reporter.log("Hi welcome",true);
+	}
+}
